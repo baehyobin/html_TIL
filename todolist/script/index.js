afe9a5ca9621b51bf65f102cbfd9c3e93c38c9fe
hyobin9824 <hyobin9824@gmail.com>
2025-12-04 12:15:41 +0900

    complete

---
+++ todolist/script/index.js
@@ -0,0 +1,4 @@
+const wrtieBtn = document.querySelector('.header_icons .write')
+const closeBtn = document.querySelector('.write_icons .close')
+const doneBtn = document.querySelector('.write_icons .done')
+
